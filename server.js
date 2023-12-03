6d0624f4cbfd6a0132d952c6519eb11cd64de77c
Rahmat Sunjani <61770721+fsdio@users.noreply.github.com>
2023-12-03 07:52:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: ["http://localhost:3000", "https://guard-connect-frontend.vercel.app"],
+    origin: ["http://localhost:3000", "https://guard-connect-backend-z64c.vercel.app/"],
   },
 });
 
